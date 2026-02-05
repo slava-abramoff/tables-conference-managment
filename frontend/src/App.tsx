222b8e695112a81ec70ca8663d76653683635fc2
slava-abramoff <vyachik005@gmail.com>
2026-02-05 12:50:39 +0300

    hotfix: delete useless import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import Layout from "./components/Layout";
 import RoleProtectedRoute from "./components/RoleProtectedRoute";
