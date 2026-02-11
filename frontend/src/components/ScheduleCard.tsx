ac22855ebd67fddb9075438ce513395a3f989ed3
slava-abramoff <vyachik005@gmail.com>
2026-02-11 12:52:02 +0300

    auto color for schedule card

--- frontend/src/components/ScheduleCard.tsx
+++ frontend/src/components/ScheduleCard.tsx
@@ -1,39 +1,36 @@
 import type { DaySchedule } from "../types/response/schedule";
 import { uniqueStrings } from "../utils/arrayUtils";
-import { formatDateRuLong } from "../utils/dateUtils";
+import { formatDateRuLong, isToday } from "../utils/dateUtils";
 
 interface ScheduleCardProps {
   data: DaySchedule;
   onClick?: () => void;
 }
 
-export default function ScheduleCard({
-  data,
-  onClick,
-}: ScheduleCardProps) {
-  const uniqueLectors = uniqueStrings(data.lectors)
-  const uniqueGroups = uniqueStrings(data.groups)
-  const ruDate = formatDateRuLong(data.date)
-
+export default function ScheduleCard({ data, onClick }: ScheduleCardProps) {
+  const color = isToday(data.date) ? "bg-green-100" : "bg-white";
+  const uniqueLectors = uniqueStrings(data.lectors);
+  const uniqueGroups = uniqueStrings(data.groups);
+  const ruDate = formatDateRuLong(data.date);
 
   return (
     <div
       onClick={onClick}
-      className="bg-white border border-slate-200 rounded-lg p-4 shadow-sm hover:shadow-md hover:border-slate-300 transition-all cursor-pointer"
+      className={`${color} border border-slate-200 rounded-lg p-4 shadow-sm hover:shadow-md hover:border-slate-300 transition-all cursor-pointer`}
     >
       <h3 className="text-lg font-semibold text-slate-900 mb-3">{ruDate}</h3>
-      
+
       <div className="space-y-2 text-sm">
         <div>
           <span className="font-medium text-slate-700">Лекторы:</span>{" "}
           <span className="text-slate-600">{uniqueLectors.join(", ")}</span>
         </div>
-        
+
         <div>
           <span className="font-medium text-slate-700">Группы:</span>{" "}
           <span className="text-slate-600">{uniqueGroups.join(", ")}</span>
         </div>
-        
+
         <div>
           <span className="font-medium text-slate-700">Кол-во занятий:</span>{" "}
           <span className="text-slate-600">{data.lectureCount}</span>
--- frontend/src/utils/dateUtils.ts
+++ frontend/src/utils/dateUtils.ts
@@ -11,3 +11,23 @@ export function formatDateRuLong(value: string): string {
     year: "numeric",
   });
 }
+
+/**
+ * Проверяет дату строкой "YYYY-MM-DD" на актуальность, true == сегодняшнее число
+ */
+export function isToday(dateStr: string): boolean {
+  if (!/^\d{4}-\d{2}-\d{2}$/.test(dateStr)) {
+    return false;
+  }
+
+  const [year, month, day] = dateStr.split("-").map(Number);
+
+  const inputDate = new Date(year, month - 1, day);
+  const today = new Date();
+
+  return (
+    inputDate.getFullYear() === today.getFullYear() &&
+    inputDate.getMonth() === today.getMonth() &&
+    inputDate.getDate() === today.getDate()
+  );
+}
